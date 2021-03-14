64ae1b9caf3836a5374052b155fb3df907ffd0ac
vladimir <vvskubiev@gmail.com>
2021-03-14 23:03:20 +0300

    refactored

--- src/main/java/com/ringcentral/testtask/parsers/CarsInfoParserImpl.java
+++ src/main/java/com/ringcentral/testtask/parsers/CarsInfoParserImpl.java
@@ -13,15 +13,10 @@ import java.util.concurrent.ConcurrentHashMap;
 public class CarsInfoParserImpl implements CarsInfoParser {
 
     private final Map<Integer, CarInfo> carsInfoMap;
-    private final CarsAPIService carsAPIService;
-    private final CarsParser carsParser;
-//    private final ExecutorService executorService = Executors.newFixedThreadPool(5);
 
     @Inject
     public CarsInfoParserImpl(CarsAPIService carsAPIService,
                               CarsParser carsParser) {
-        this.carsParser = carsParser;
-        this.carsAPIService = carsAPIService;
         carsInfoMap = new ConcurrentHashMap<>();
         List<Integer> carIdList = carsParser.getAllCarsId();
         carIdList.stream().parallel().forEach(carId -> carsInfoMap.putIfAbsent(carId, carsAPIService.getCarById(carId)));
@@ -31,27 +26,5 @@ public class CarsInfoParserImpl implements CarsInfoParser {
     public Map<Integer, CarInfo> getCarsInfoMap() {
         return carsInfoMap;
     }
-
-//    List<CarInfo> getCarInfos(List<Integer> carIdList) {
-//        CountDownLatch countDownLatch = new CountDownLatch(carIdList.size());
-//        List<CarInfo> carInfoList = new CopyOnWriteArrayList<>();
-//
-//        for (int i = 0; i < carIdList.size(); ++i) {
-//            final int finalI = i;
-//            executorService.submit(() -> {
-//                carInfoList.add(carsAPIService.getCarById(carIdList.get(finalI)));
-//                countDownLatch.countDown();
-//            });
-//        }
-//        try {
-//            countDownLatch.await();
-//        } catch (final InterruptedException e) {
-//            e.printStackTrace();
-//            return new ArrayList<>();
-//        }
-//
-//        return carInfoList;
-//    }
-
 
 }
